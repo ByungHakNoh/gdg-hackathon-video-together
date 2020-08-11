7394429089af91b7578ae947c020c55d531328fe
ByungHakNoh <nbh6579@naver.com>
2020-08-11 14:11:33 +0900

    [add] EntityMapper : Entity와 도메인 데이터 mapping하는 인터페이스 추가

---
+++ app/src/main/java/org/personal/videotogether/util/EntityMapper.kt
@@ -0,0 +1,6 @@
+package org.personal.videotogether.util
+
+interface EntityMapper<Entity, DomainModel> {
+    fun mapFromEntity(entity:Entity) : DomainModel
+    fun mapToEntity (domainModel: DomainModel) : Entity
+}

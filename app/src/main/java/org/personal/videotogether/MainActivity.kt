649c9fe01d2a6b6c0119fdba70b4be2569fc3359
ByungHakNoh <nbh6579@naver.com>
2020-08-11 14:19:02 +0900

    [add] MainActivity : 메인 액티비티 hilt annotation 추가

--- app/src/main/java/org/personal/videotogether/MainActivity.kt
+++ app/src/main/java/org/personal/videotogether/MainActivity.kt
@@ -3,9 +3,12 @@ package org.personal.videotogether
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

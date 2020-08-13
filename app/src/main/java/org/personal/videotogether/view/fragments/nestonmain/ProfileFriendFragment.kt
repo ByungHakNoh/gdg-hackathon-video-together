9444ec341adb42c0925ca78a6c310a2aa45b3c46
ByungHakNoh <nbh6579@naver.com>
2020-08-14 00:44:36 +0900

    [add] ProfileFriendFragment : 추가

---
+++ app/src/main/java/org/personal/videotogether/view/fragments/nestonmain/ProfileFriendFragment.kt
@@ -0,0 +1,55 @@
+package org.personal.videotogether.view.fragments.nestonmain
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_profile_friend.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.personal.videotogether.R
+import org.personal.videotogether.domianmodel.FriendData
+
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class ProfileFriendFragment : Fragment(R.layout.fragment_profile_friend), View.OnClickListener {
+
+    private val TAG = javaClass.name
+
+    private lateinit var mainNavController: NavController
+
+    private val argument: ProfileFriendFragmentArgs by navArgs()
+    private val friendData : FriendData by lazy { argument.friendData }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainNavController = Navigation.findNavController(view)
+        setInitView()
+        setListener()
+    }
+
+    private fun setInitView() {
+        Glide.with(requireContext()).load(friendData.profileImageUrl).into(profileIV)
+        nameTV.text = friendData.name
+    }
+
+    private fun setListener() {
+        closeBtn.setOnClickListener(this)
+        chatBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.closeBtn -> {
+                requireActivity().onBackPressed()
+            }
+            R.id.chatBtn -> {
+                mainNavController.navigate(R.id.action_profileFriendFragment_to_chattingFragment)
+            }
+        }
+    }
+}

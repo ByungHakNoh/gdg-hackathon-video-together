2e56f244036b8205523cd637ca88278b0dd999d6
ByungHakNoh <nbh6579@naver.com>
2020-08-14 00:35:52 +0900

    [modify] RequestData : 패키지 명 변경

--- app/src/main/java/org/personal/videotogether/server/RequestData.kt
+++ app/src/main/java/org/personal/videotogether/server/RequestData.kt
@@ -1,4 +1,4 @@
-package org.personal.videotogether.model.server
+package org.personal.videotogether.server
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName

567b19df2d7a70a67a3933a84711f43b4f7d87dd
ByungHakNoh <nbh6579@naver.com>
2020-08-14 00:44:44 +0900

    [add] ProfileMineFragment : 추가

---
+++ app/src/main/java/org/personal/videotogether/view/fragments/nestonmain/ProfileMineFragment.kt
@@ -0,0 +1,55 @@
+package org.personal.videotogether.view.fragments.nestonmain
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_profile_mine.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.personal.videotogether.R
+import org.personal.videotogether.viewmodel.UserViewModel
+
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class ProfileMineFragment : Fragment(R.layout.fragment_profile_mine), View.OnClickListener {
+
+    private val TAG = javaClass.name
+
+    private lateinit var mainNavController: NavController
+
+    private val userViewModel: UserViewModel by lazy { ViewModelProvider(requireActivity())[UserViewModel::class.java] }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainNavController = Navigation.findNavController(view)
+        setInitView()
+        setListener()
+    }
+
+    private fun setInitView() {
+        val userData = userViewModel.userData.value!!
+        Glide.with(requireContext()).load(userData.profileImageUrl).into(profileIV)
+        nameTV.text = userData.name
+    }
+
+    private fun setListener() {
+        closeBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.closeBtn -> {
+                requireActivity().onBackPressed()
+            }
+            R.id.chatBtn -> {
+                // TODO : 채팅방으로 넘어가기
+            }
+        }
+    }
+
+}

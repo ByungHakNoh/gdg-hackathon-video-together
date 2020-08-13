7bd7ca24c1c440d95b1cee03b7263f80f5d6aecd
ByungHakNoh <nbh6579@naver.com>
2020-08-14 00:48:35 +0900

    [add] MainActivity : view model 추가, 뒤로가기 버튼 예외처리

--- app/src/main/java/org/personal/videotogether/MainActivity.kt
+++ app/src/main/java/org/personal/videotogether/MainActivity.kt
@@ -1,16 +1,47 @@
 package org.personal.videotogether
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main_home.view.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.personal.videotogether.viewmodel.ChatViewModel
+import org.personal.videotogether.viewmodel.FriendViewModel
+import org.personal.videotogether.viewmodel.UserViewModel
+import org.personal.videotogether.viewmodel.YoutubeViewModel
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val userViewModel: UserViewModel by viewModels()
+    private val friendViewModel: FriendViewModel by viewModels()
+    private val youtubeViewModel: YoutubeViewModel by viewModels()
+    private val chatViewModel: ChatViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onBackPressed() {
+        if (mainFragmentContainer.homeFragmentContainer != null) {
+            val homeNavController = Navigation.findNavController(mainFragmentContainer.homeFragmentContainer)
+
+            if (homeNavController.backStack.count() > 2) {
+                homeNavController.popBackStack()
+            } else {
+                moveTaskToBack(true)
+                finishAndRemoveTask()
+            }
+        } else {
+            super.onBackPressed()
+        }
     }
 }

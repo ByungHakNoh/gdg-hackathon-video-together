3955938d09feda767f18501003d9b17298144662
ByungHakNoh <nbh6579@naver.com>
2020-08-14 00:42:47 +0900

    [add] YoutubeSearchFragment : 추가

---
+++ app/src/main/java/org/personal/videotogether/view/fragments/home/nestonhome/YoutubeSearchFragment.kt
@@ -0,0 +1,13 @@
+package org.personal.videotogether.view.fragments.home.nestonhome
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.personal.videotogether.R
+
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class YoutubeSearchFragment : Fragment(R.layout.fragment_youtube_search) {
+
+
+}

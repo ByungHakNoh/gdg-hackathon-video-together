140e5df340e121aba4ea92adc497ffe7653e9364
ByungHakNoh <nbh6579@naver.com>
2020-08-14 00:39:00 +0900

    [add] DataState : NoData, DuplicatedData 상태 클래스 추가

--- app/src/main/java/org/personal/videotogether/util/DataState.kt
+++ app/src/main/java/org/personal/videotogether/util/DataState.kt
@@ -4,7 +4,8 @@ import java.lang.Exception
 
 sealed class DataState<out R> {
     data class Success<out T>(val data: T) : DataState<T>()
-    data class ResponseError(val serverError: String) : DataState<Nothing>()
+    data class NoData(val serverError: String) : DataState<Nothing>()
+    object DuplicatedData: DataState<Nothing>()
     data class Error(val exception: Exception) : DataState<Nothing>()
     object Loading: DataState<Nothing>()
 }

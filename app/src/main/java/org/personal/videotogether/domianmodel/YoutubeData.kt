5b45933c1b6054429cde201b327ff4dc53d54010
ByungHakNoh <nbh6579@naver.com>
2020-08-14 00:27:47 +0900

    [add] YoutubeData : 유투브 데이터 추가

---
+++ app/src/main/java/org/personal/videotogether/domianmodel/YoutubeData.kt
@@ -0,0 +1,8 @@
+package org.personal.videotogether.domianmodel
+
+data class YoutubeData (
+    val channelTitle: String,
+    val channelThumbnail: String,
+    val title : String,
+    val videoId : String
+)

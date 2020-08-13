090c61f96a2c91a1ea62468f2f2d77cd323cae0b
ByungHakNoh <nbh6579@naver.com>
2020-08-14 00:41:32 +0900

    [add] ChoiceDialog : 추가

---
+++ app/src/main/java/org/personal/videotogether/view/dialog/ChoiceDialog.kt
@@ -0,0 +1,49 @@
+package org.personal.videotogether.view.dialog
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.DialogFragment
+
+class ChoiceDialog : DialogFragment(), DialogInterface.OnClickListener {
+    private val TAG = javaClass.name
+
+    private lateinit var dialogListener: DialogListener
+    private val menuList: Int by lazy { requireArguments().getInt("arrayResource") }
+    private var itemPosition: Int? = null
+    private var id :Int? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        itemPosition = arguments?.getInt("itemPosition")
+        id = arguments?.getInt("id")
+
+        return AlertDialog.Builder(context)
+            .setItems(menuList, this)
+            .create()
+    }
+
+    // 다이얼로그에서 액티비티로 데이터 전송
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+
+            dialogListener =  requireParentFragment() as DialogListener
+
+        } catch (e: ClassCastException) {
+
+            e.printStackTrace()
+            Log.i(TAG, "onAttach : 인터페이스 implement 해야 함")
+        }
+    }
+
+    interface DialogListener {
+        fun onChoice(whichDialog: Int, choice: String, itemPosition: Int?, id:Int?)
+    }
+
+    override fun onClick(dialog: DialogInterface?, which: Int) {
+        dialogListener.onChoice(menuList, requireParentFragment().resources.getStringArray(menuList)[which], itemPosition, id)
+    }
+}

a49b1234fb5e84307b8062406a3d629bea8f30ff
ByungHakNoh <nbh6579@naver.com>
2020-08-14 00:47:32 +0900

    [sort] 코드 정렬

--- app/src/main/java/org/personal/videotogether/viewmodel/UserStateEvent.kt
+++ app/src/main/java/org/personal/videotogether/viewmodel/UserStateEvent.kt
@@ -1,6 +1,7 @@
 package org.personal.videotogether.viewmodel
 
 sealed class UserStateEvent {
+    object GetUserDataFromLocal : UserStateEvent()
     class CheckDuplicatedEmail(val email: String) : UserStateEvent()
     class UploadUser(val email: String, val password: String) : UserStateEvent()
     class UploadUserProfile(val base64Image: String, val name: String) : UserStateEvent()
--- app/src/main/java/org/personal/videotogether/viewmodel/UserViewModel.kt
+++ app/src/main/java/org/personal/videotogether/viewmodel/UserViewModel.kt
@@ -7,7 +7,8 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
-import org.personal.videotogether.model.repository.UserRepository
+import org.personal.videotogether.domianmodel.UserData
+import org.personal.videotogether.repository.UserRepository
 import org.personal.videotogether.util.DataState
 
 @ExperimentalCoroutinesApi
@@ -17,6 +18,9 @@ constructor(
     private val userRepository: UserRepository,
     @Assisted private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
+
+    private val _userData: MutableLiveData<UserData?> = MutableLiveData()
+    val userData: LiveData<UserData?> get() = _userData
 
     // ------------------ Sign Up live data ------------------
     private val _validationDataState: MutableLiveData<DataState<Boolean?>> = MutableLiveData()
@@ -30,22 +34,30 @@ constructor(
     val uploadUserProfileState: LiveData<DataState<Boolean?>> get() = _uploadUserProfileState
 
     // ------------------ Sign In live data ------------------
-    private val _signInState: MutableLiveData<DataState<Boolean?>> = MutableLiveData()
-    val signInState: LiveData<DataState<Boolean?>> get() = _signInState
+    private val _signInState: MutableLiveData<DataState<UserData?>> = MutableLiveData()
+    val signInState: LiveData<DataState<UserData?>> get() = _signInState
 
     fun setStateEvent(userStateEvent: UserStateEvent) {
         viewModelScope.launch {
             when (userStateEvent) {
+                is UserStateEvent.GetUserDataFromLocal -> {
+                    userRepository.getUserDataFromLocal().onEach { dataState ->
+                        _userData.value = dataState
+                    }.launchIn(viewModelScope)
+                }
+
                 // ------------------ Sign Up ------------------
                 is UserStateEvent.CheckDuplicatedEmail -> {
                     userRepository.postCheckEmailValid(userStateEvent.email).onEach { dataState ->
                         _validationDataState.value = dataState
+                        _validationDataState.value = null
                     }.launchIn(viewModelScope)
                 }
 
                 is UserStateEvent.UploadUser -> {
                     userRepository.uploadUser(userStateEvent.email, userStateEvent.password).onEach { dataState ->
                         _uploadUserDataState.value = dataState
+                        _uploadUserDataState.value = null
                     }.launchIn(viewModelScope)
                 }
 
@@ -53,6 +65,7 @@ constructor(
                 is UserStateEvent.UploadUserProfile -> {
                     userRepository.uploadUserProfile(userStateEvent.base64Image, userStateEvent.name).onEach { dataState ->
                         _uploadUserProfileState.value = dataState
+                        _uploadUserProfileState.value = null
                     }.launchIn(viewModelScope)
                 }
 
@@ -60,6 +73,7 @@ constructor(
                 is UserStateEvent.SignIn -> {
                     userRepository.signIn(userStateEvent.email, userStateEvent.password).onEach { dataState ->
                         _signInState.value = dataState
+                        _signInState.value = null
                     }.launchIn(viewModelScope)
                 }
             }

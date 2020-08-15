edb1dc74757d9a1b96f90d5aa17cb503c0154978
ByungHakNoh <nbh6579@naver.com>
2020-08-15 16:53:03 +0900

    [add] VideoTogetherDatabase : ChatRoom 테이블 추가

--- app/src/main/java/org/personal/videotogether/room/VideoTogetherDatabase.kt
+++ app/src/main/java/org/personal/videotogether/room/VideoTogetherDatabase.kt
@@ -2,18 +2,23 @@ package org.personal.videotogether.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import org.personal.videotogether.room.entity.ChatRoomCacheEntity
 import org.personal.videotogether.room.entity.FriendCacheEntity
 import org.personal.videotogether.room.entity.UserCacheEntity
 
 @Database(
     entities = [
         UserCacheEntity::class,
-        FriendCacheEntity::class
-    ], version = 3
+        FriendCacheEntity::class,
+        ChatRoomCacheEntity::class
+    ],
+    version = 4,
+    exportSchema = false
 )
 abstract class VideoTogetherDatabase : RoomDatabase() {
     abstract fun userDAO(): UserDAO
     abstract fun friendDAO(): FriendDAO
+    abstract fun chatRoomDAO(): ChatRoomDAO
 
     companion object {
         const val DATABASE_NAME: String = "video_together"

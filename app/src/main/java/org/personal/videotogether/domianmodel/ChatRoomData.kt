8e22a95e1f8262527e8020d606fb95b10223632d
ByungHakNoh <nbh6579@naver.com>
2020-08-15 22:41:07 +0900

    [add] ChatRoomData : Parcelable 추가

--- app/src/main/java/org/personal/videotogether/domianmodel/ChatRoomData.kt
+++ app/src/main/java/org/personal/videotogether/domianmodel/ChatRoomData.kt
@@ -1,7 +1,11 @@
 package org.personal.videotogether.domianmodel
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class ChatRoomData (
     val id : Int,
     val lastChatMessage: String?,
     val participantList: List<UserData>
-)
+) : Parcelable

30a1f9b5340ff2ef90f24c55138aae4c61c4a64a
ByungHakNoh <nbh6579@naver.com>
2020-08-15 16:51:42 +0900

    [add] ChatRoomData : 채팅 방 데이터 추가

--- app/src/main/java/org/personal/videotogether/domianmodel/ChatRoomData.kt
+++ app/src/main/java/org/personal/videotogether/domianmodel/ChatRoomData.kt
@@ -1,9 +1,7 @@
 package org.personal.videotogether.domianmodel
 
-import org.personal.videotogether.server.entity.UserEntity
-
 data class ChatRoomData (
     val id : Int,
     val lastChatMessage: String?,
-    val participantsList: List<UserEntity>
+    val participantList: List<UserData>
 )

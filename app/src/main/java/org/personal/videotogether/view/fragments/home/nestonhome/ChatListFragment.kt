00ed0e5ed286d3ba3f56fd094b718e24c9f2908a
ByungHakNoh <nbh6579@naver.com>
2020-08-16 01:30:02 +0900

    [delete] ChatListFragment : chatRoomList(live data) 관찰하는 코드 추가

--- app/src/main/java/org/personal/videotogether/view/fragments/home/nestonhome/ChatListFragment.kt
+++ app/src/main/java/org/personal/videotogether/view/fragments/home/nestonhome/ChatListFragment.kt
@@ -1,8 +1,10 @@
 package org.personal.videotogether.view.fragments.home.nestonhome
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -16,6 +18,7 @@ import org.personal.videotogether.domianmodel.ChatRoomData
 import org.personal.videotogether.util.DataState
 import org.personal.videotogether.util.view.DataStateHandler
 import org.personal.videotogether.view.adapter.ChatRoomAdapter
+import org.personal.videotogether.view.fragments.nestonmain.HomeFragmentDirections
 import org.personal.videotogether.viewmodel.ChatStateEvent
 import org.personal.videotogether.viewmodel.ChatViewModel
 import org.personal.videotogether.viewmodel.UserViewModel
@@ -23,11 +26,14 @@ import org.personal.videotogether.viewmodel.UserViewModel
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class ChatListFragment
-    constructor(
-        private val dataStateHandler: DataStateHandler
-    ): Fragment(R.layout.fragment_chat_list), ChatRoomAdapter.ItemClickListener {
+constructor(
+    private val dataStateHandler: DataStateHandler
+) : Fragment(R.layout.fragment_chat_list), ChatRoomAdapter.ItemClickListener {
 
     private val TAG by lazy { javaClass.name }
+
+    private lateinit var mainNavController: NavController
+
 
     private val userViewModel: UserViewModel by lazy { ViewModelProvider(requireActivity())[UserViewModel::class.java] }
     private val chatViewModel by lazy { ViewModelProvider(requireActivity())[ChatViewModel::class.java] }
@@ -38,29 +44,37 @@ class ChatListFragment
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mainFragmentContainer: FragmentContainerView = view.rootView.findViewById(R.id.mainFragmentContainer)
+        mainNavController = Navigation.findNavController(mainFragmentContainer)
         subscribeObservers()
         buildRecyclerView()
-        chatViewModel.setStateEvent(ChatStateEvent.GetChatRoomList(userViewModel.userData.value!!.id))
+        chatViewModel.setStateEvent(ChatStateEvent.GetChatRoomsFromServer(userViewModel.userData.value!!.id))
     }
 
     private fun subscribeObservers() {
+
+        chatViewModel.chatRoomList.observe(viewLifecycleOwner, Observer { localChatRoomList ->
+            chatRoomList.clear()
+            localChatRoomList!!.forEach { chatRoomData ->
+                chatRoomList.add(chatRoomData)
+            }
+            chatRoomAdapter.notifyDataSetChanged()
+        })
+
         // 친구 검색하기
         chatViewModel.getChatRoomList.observe(viewLifecycleOwner, Observer { dataState ->
             when (dataState) {
                 is DataState.Loading -> {
-                    dataStateHandler.displayLoadingDialog(true, childFragmentManager)
+                    Log.i(TAG, "getChatRoomList: 로딩")
                 }
                 is DataState.Success<List<ChatRoomData>?> -> {
-                    dataStateHandler.displayLoadingDialog(false, childFragmentManager)
-                    chatRoomList.clear()
-                    dataState.data!!.forEach { chatRoomData -> chatRoomList.add(chatRoomData) }
-                    chatRoomAdapter.notifyDataSetChanged()
+                    Log.i(TAG, "getChatRoomList: 성공")
                 }
                 is DataState.NoData -> {
-                    dataStateHandler.displayLoadingDialog(false, childFragmentManager)
+                    Log.i(TAG, "getChatRoomList: 데이터 없음")
                 }
                 is DataState.Error -> {
-                    dataStateHandler.displayLoadingDialog(false, childFragmentManager)
+                    Log.i(TAG, "getChatRoomList: 에러 발생")
                 }
             }
         })
@@ -75,6 +89,8 @@ class ChatListFragment
     }
 
     override fun onItemClick(view: View?, itemPosition: Int) {
-
+        val selectedChatRoom = chatRoomList[itemPosition]
+        val action = HomeFragmentDirections.actionMainHomeFragmentToChattingFragment(selectedChatRoom)
+        mainNavController.navigate(action)
     }
 }

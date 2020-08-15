3845905d01c192b9f0c84934f06d462d9e14e1c3
ByungHakNoh <nbh6579@naver.com>
2020-08-15 16:56:00 +0900

    [add] ChatStateEvent, ChatViewModel : 채팅상태 이벤트(채팅방 가져오기, 소켓 연결) 추가

--- app/src/main/java/org/personal/videotogether/viewmodel/ChatStateEvent.kt
+++ app/src/main/java/org/personal/videotogether/viewmodel/ChatStateEvent.kt
@@ -4,6 +4,11 @@ import org.personal.videotogether.domianmodel.FriendData
 import org.personal.videotogether.domianmodel.UserData
 
 sealed class ChatStateEvent{
+    object GetChatRoomsFromLocal : ChatStateEvent()
+    class GetChatRoomsFromServer(val userId: Int): ChatStateEvent()
     class AddChatRoom(val userData: UserData, val participantList: List<FriendData>) : ChatStateEvent()
-    class GetChatRoomList(val userId: Int): ChatStateEvent()
+    // tcp 관련
+    object ConnectToTCPServer : ChatStateEvent()
+    class SendToTCPServer(val message:String) :ChatStateEvent()
+    object ReceiveFromTCPServer : ChatStateEvent()
 }
--- app/src/main/java/org/personal/videotogether/viewmodel/ChatViewModel.kt
+++ app/src/main/java/org/personal/videotogether/viewmodel/ChatViewModel.kt
@@ -3,10 +3,13 @@ package org.personal.videotogether.viewmodel
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.personal.videotogether.domianmodel.ChatRoomData
 import org.personal.videotogether.repository.ChatRepository
 import org.personal.videotogether.util.DataState
@@ -19,16 +22,45 @@ constructor(
     @Assisted private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    // ------------------ Add Friend live data ------------------
+    private val _chatRoomList: MutableLiveData<List<ChatRoomData>?> = MutableLiveData()
+    val chatRoomList: LiveData<List<ChatRoomData>?> get() = _chatRoomList
+
+    // ------------------ Add Chat Room live data ------------------
     private val _addChatRoom: MutableLiveData<DataState<ChatRoomData?>> = MutableLiveData()
     val addChatRoom: LiveData<DataState<ChatRoomData?>> get() = _addChatRoom
 
+    // ------------------ Add Chat Room live data ------------------
     private val _getChatRoomList: MutableLiveData<DataState<List<ChatRoomData>?>> = MutableLiveData()
     val getChatRoomList: LiveData<DataState<List<ChatRoomData>?>> get() = _getChatRoomList
+
+    // ------------------ TCP 통신 live data ------------------
+    private val _connectToTCPServer: MutableLiveData<Boolean?> = MutableLiveData()
+    val connectToTCPServer: LiveData<Boolean?> get() = _connectToTCPServer
+
+    private val _receivedMessage: MutableLiveData<String?> = MutableLiveData()
+    val receivedMessage: LiveData<String?> get() = _receivedMessage
+
+    private val _sendMessage: MutableLiveData<DataState<Boolean>?> = MutableLiveData()
+    val sendMessage: LiveData<DataState<Boolean>?> get() = _sendMessage
 
     fun setStateEvent(chatStateEvent: ChatStateEvent) {
         viewModelScope.launch {
             when (chatStateEvent) {
+
+                is ChatStateEvent.GetChatRoomsFromLocal -> {
+                    chatRepository.getChatRoomFromLocal().onEach { dataState ->
+                        _chatRoomList.value = dataState
+                    }.launchIn(viewModelScope)
+                }
+
+                is ChatStateEvent.GetChatRoomsFromServer -> {
+                    chatRepository.getChatRoomsFromServer(chatStateEvent.userId).onEach { dataState ->
+                        _getChatRoomList.value = dataState
+
+                        // 서버에서 데이터를 가져오면 채팅방 live data 업데이트
+                        if (dataState is DataState.Success) _chatRoomList.value = dataState.data
+                    }.launchIn(viewModelScope)
+                }
 
                 is ChatStateEvent.AddChatRoom -> {
                     chatRepository.addChatRoom(chatStateEvent.userData, chatStateEvent.participantList).onEach { dataState ->
@@ -37,9 +69,21 @@ constructor(
                     }.launchIn(viewModelScope)
                 }
 
-                is ChatStateEvent.GetChatRoomList -> {
-                    chatRepository.getChatRoomList(chatStateEvent.userId).onEach { dataState ->
-                        _getChatRoomList.value = dataState
+                is ChatStateEvent.ConnectToTCPServer -> {
+                    chatRepository.connectToTCPServer()
+                }
+
+                is ChatStateEvent.ReceiveFromTCPServer -> {
+                    withContext(Dispatchers.Default) {
+                        chatRepository.receiveFromTCPServer().onEach { dataState ->
+                            _receivedMessage.value = dataState
+                        }.launchIn(viewModelScope)
+                    }
+                }
+
+                is ChatStateEvent.SendToTCPServer -> {
+                    chatRepository.sendToTCPServer(chatStateEvent.message).onEach { dataState ->
+                        _sendMessage.value = dataState
                     }.launchIn(viewModelScope)
                 }
             }

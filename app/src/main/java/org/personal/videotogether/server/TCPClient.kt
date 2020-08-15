3380a86b837921c12d1b2ff3b2b14a1e9774b347
ByungHakNoh <nbh6579@naver.com>
2020-08-15 16:52:29 +0900

    [add] TCPClient : 소켓 클라이언트 클래스 추가

---
+++ app/src/main/java/org/personal/videotogether/server/TCPClient.kt
@@ -0,0 +1,60 @@
+package org.personal.videotogether.server
+
+import android.util.Log
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import java.io.*
+import java.net.Socket
+
+class TCPClient(private val serverName: String, private val serverPort: Int) {
+
+    private val TAG by lazy { javaClass.name }
+
+    private var socket: Socket? = null
+    private var serverInPut: InputStream? = null
+    private var serverOutPut: OutputStream? = null
+    var bufferedReader: BufferedReader? = null
+
+    // 서버와 연결을 확인하는 메소드
+    fun connect(): Boolean {
+        try {
+            socket = Socket(serverName, serverPort)
+            serverOutPut = socket!!.getOutputStream()
+            serverInPut = socket!!.getInputStream()
+            bufferedReader = BufferedReader(InputStreamReader(serverInPut!!))
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.i(TAG, "TCPClient connect : IOException 발생")
+        }
+        return true
+    }
+
+    // 커플 ID - 나의 ID - 이름 - 이미지 url - message
+    fun writeMessage(message: String) {
+        val writer = PrintWriter(serverOutPut!!)
+        writer.println(message)
+        writer.flush()
+    }
+
+    fun readMessage(): String? {
+        if (!socket!!.isClosed) {
+            val response: String? = bufferedReader?.readLine()
+            if (response != null) {
+                Log.i(TAG, "제대로 : $response")
+                Log.i(TAG, "메시지 전송 완료")
+            }
+            return response
+        }
+        return null
+    }
+
+    fun socketClose() {
+        Log.i(TAG, socket?.isClosed.toString())
+        serverInPut?.close()
+        serverOutPut?.close()
+        bufferedReader?.close()
+        socket?.close()
+        Log.i(TAG, socket?.isClosed.toString())
+    }
+}
+

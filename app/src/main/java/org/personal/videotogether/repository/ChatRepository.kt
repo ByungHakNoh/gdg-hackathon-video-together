ef078dd8b061e8eb8d764392dae835b7f4f30d2e
ByungHakNoh <nbh6579@naver.com>
2020-08-16 01:28:16 +0900

    [delete] ChatRepository : 소켓 통신 관련 메소드 유투브에서도 사용하기 때문에 따로 클래스 생성해서 사용

--- app/src/main/java/org/personal/videotogether/repository/ChatRepository.kt
+++ app/src/main/java/org/personal/videotogether/repository/ChatRepository.kt
@@ -10,7 +10,6 @@ import org.personal.videotogether.room.ChatRoomDAO
 import org.personal.videotogether.room.entity.ChatRoomCacheMapper
 import org.personal.videotogether.server.RequestData
 import org.personal.videotogether.server.RetrofitRequest
-import org.personal.videotogether.server.TCPClient
 import org.personal.videotogether.server.entity.ChatRoomMapper
 import org.personal.videotogether.server.entity.FriendMapper
 import org.personal.videotogether.server.entity.UserMapper
@@ -27,16 +26,6 @@ constructor(
     private val friendMapper: FriendMapper
 ) {
     private val TAG by lazy { javaClass.name }
-
-    private lateinit var tcpClient: TCPClient
-    private var isTCPClientStop = false
-    private var isSocketRegistered = false
-    private var isReceivingMessage = false
-
-    interface SocketListener {
-        fun onChatMessage(chatData: String)
-        fun onYoutubeMessage(youtubeData: String)
-    }
 
     suspend fun getChatRoomFromLocal(): Flow<List<ChatRoomData>?> = flow {
         try {
@@ -105,81 +94,5 @@ constructor(
             Log.e(TAG, "postCheckEmailValid: 서버 통신 에러발생($e)")
             emit(DataState.Error(e))
         }
-    }
-
-    // ------------------ TCP 통신 관련 메소드 모음 ------------------
-    fun connectToTCPServer() {
-        try {
-            tcpClient = TCPClient("3.34.22.62", 20205)
-
-            if (tcpClient.connect()) Log.e(TAG, "connectToTCPServer: 연결 완료")
-            else Log.e(TAG, "connectToTCPServer: tcp 서버 연결 안됨")
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.e(TAG, "connectToTCPServer: tcp 서버 통신 에러발생($e)")
-        }
-    }
-
-    fun disConnectFromTCPServer() {
-        tcpClient.writeMessage("quit")
-        isTCPClientStop = true
-    }
-
-    fun registerSocket(userData: UserData) {
-        if (isSocketRegistered) return
-        isSocketRegistered = true
-
-        val socketInfo = "registerUserInfo@${userData.id}@${userData.name}@${userData.profileImageUrl}"
-        Log.i(TAG, "registerSocket: registered")
-        tcpClient.writeMessage(socketInfo)
-    }
-
-    fun receiveFromTCPServer(socketListener: SocketListener) {
-        if (isReceivingMessage) return
-        isReceivingMessage = true
-
-        Thread(Runnable {
-            try {
-                Log.i(TAG, "receiveFromTCPServer: thread 시작")
-
-                while (!isTCPClientStop) {
-
-                    val respond = tcpClient.readMessage()
-
-                    // 서버에서 읽어드린 메시지가 있으면 Flag 확인
-                    if (!respond.isNullOrEmpty()) {
-
-                        val splitMessageData = respond.split("@")
-
-                        // 서버에서 보낸 flag 확인
-                        when (splitMessageData[0]) {
-                            "chat" -> socketListener.onChatMessage(respond)
-                            "youtube" -> socketListener.onYoutubeMessage(respond)
-                        }
-                    }
-                }
-                tcpClient.socketClose()
-                isReceivingMessage = false
-                Log.i(TAG, "receiveFromTCPServer: thread 종료")
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Log.e(TAG, "receiveFromTCPServer: tcp 서버 통신 에러발생($e)")
-            }
-        }).start()
-    }
-
-    fun sendToTCPServer(flag: String, roomId: String?, message: String?) {
-
-        val formedMessage = "$flag@$roomId@$message"
-
-        tcpClient.writeMessage(formedMessage)
-        Log.i(TAG, "전송 완료")
-    }
-
-    companion object {
-        const val JOIN_CHAT_ROOM = "joinChatRoom"
-        const val EXIT_CHAT_ROOM = "exitChatRoom"
-        const val SEND_CHAT_MESSAGE = "sendChatMessage"
     }
 }

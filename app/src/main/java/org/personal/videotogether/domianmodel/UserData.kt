db8b73bb00c8e7301cde2fc6ccdc2f0a0556b233
ByungHakNoh <nbh6579@naver.com>
2020-08-15 22:41:18 +0900

    [add] UserData : Parcelable 추가

--- app/src/main/java/org/personal/videotogether/domianmodel/UserData.kt
+++ app/src/main/java/org/personal/videotogether/domianmodel/UserData.kt
@@ -1,9 +1,13 @@
 package org.personal.videotogether.domianmodel
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class UserData(
     val id : Int,
     val email: String,
     val password: String,
     val name : String?,
     val profileImageUrl: String?
-)
+) : Parcelable

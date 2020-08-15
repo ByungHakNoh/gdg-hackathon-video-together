da2b44705f7e63497ca859edd96674aff8fce7f9
ByungHakNoh <nbh6579@naver.com>
2020-08-15 16:59:45 +0900

    [sort] 코드 정렬

--- app/src/main/java/org/personal/videotogether/viewmodel/ChatStateEvent.kt
+++ app/src/main/java/org/personal/videotogether/viewmodel/ChatStateEvent.kt
@@ -3,12 +3,13 @@ package org.personal.videotogether.viewmodel
 import org.personal.videotogether.domianmodel.FriendData
 import org.personal.videotogether.domianmodel.UserData
 
-sealed class ChatStateEvent{
+sealed class ChatStateEvent {
     object GetChatRoomsFromLocal : ChatStateEvent()
-    class GetChatRoomsFromServer(val userId: Int): ChatStateEvent()
+    class GetChatRoomsFromServer(val userId: Int) : ChatStateEvent()
     class AddChatRoom(val userData: UserData, val participantList: List<FriendData>) : ChatStateEvent()
+
     // tcp 관련
     object ConnectToTCPServer : ChatStateEvent()
-    class SendToTCPServer(val message:String) :ChatStateEvent()
+    class SendToTCPServer(val message: String) : ChatStateEvent()
     object ReceiveFromTCPServer : ChatStateEvent()
 }

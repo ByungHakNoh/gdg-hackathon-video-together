7c8b503ae534faeaf15af17ef21f18e50cbed49e
ByungHakNoh <nbh6579@naver.com>
2020-08-16 01:33:41 +0900

    [add] MainActivity : 소켓 통신 뷰 모델 추가

--- app/src/main/java/org/personal/videotogether/MainActivity.kt
+++ app/src/main/java/org/personal/videotogether/MainActivity.kt
@@ -3,17 +3,13 @@ package org.personal.videotogether
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main_home.view.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.personal.videotogether.viewmodel.ChatViewModel
-import org.personal.videotogether.viewmodel.FriendViewModel
-import org.personal.videotogether.viewmodel.UserViewModel
-import org.personal.videotogether.viewmodel.YoutubeViewModel
+import org.personal.videotogether.viewmodel.*
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
@@ -23,10 +19,12 @@ class MainActivity : AppCompatActivity() {
     private val friendViewModel: FriendViewModel by viewModels()
     private val youtubeViewModel: YoutubeViewModel by viewModels()
     private val chatViewModel: ChatViewModel by viewModels()
+    private val socketViewModel: SocketViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        socketViewModel.setStateEvent(SocketStateEvent.ConnectToTCPServer)
     }
 
     @SuppressLint("RestrictedApi")
@@ -37,11 +35,17 @@ class MainActivity : AppCompatActivity() {
             if (homeNavController.backStack.count() > 2) {
                 homeNavController.popBackStack()
             } else {
-                moveTaskToBack(true)
-                finishAndRemoveTask()
+                killProcess()
             }
         } else {
             super.onBackPressed()
         }
+    }
+
+    private fun killProcess() {
+        moveTaskToBack(true)
+        finishAndRemoveTask()
+        // TODO : 뒤로가기 버튼으로 액티비티 스택을 지우면 SocketViewModel onCleared 에서 tcp disconnect 가 호출 되지 않음 -> 해결방안 찾기
+        socketViewModel.setStateEvent(SocketStateEvent.DisconnectFromTCPServer)
     }
 }

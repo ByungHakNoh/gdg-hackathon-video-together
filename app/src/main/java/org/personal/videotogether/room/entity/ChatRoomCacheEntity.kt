3335a8b6d80323475940d2d66d971bf4d3122957
ByungHakNoh <nbh6579@naver.com>
2020-08-15 22:37:21 +0900

    [add] ChatRoomCacheEntity : 컬럼 값 추가

--- app/src/main/java/org/personal/videotogether/room/entity/ChatRoomCacheEntity.kt
+++ app/src/main/java/org/personal/videotogether/room/entity/ChatRoomCacheEntity.kt
@@ -1,7 +1,18 @@
 package org.personal.videotogether.room.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "chat_room")
-class ChatRoomCacheEntity {
-}
+data class ChatRoomCacheEntity (
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "id")
+    val id : Int,
+
+    @ColumnInfo(name = "last_chat_message")
+    val last_chat_message : String?,
+
+    @ColumnInfo(name = "participant_list")
+    val participant_list : String?
+)

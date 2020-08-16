a5bc1e869354719bde5861a103346e29019cc911
ByungHakNoh <nbh6579@naver.com>
2020-08-16 18:10:29 +0900

    [add] HomeDetailBlankFragment : 추가

---
+++ app/src/main/java/org/personal/videotogether/view/fragments/home/nestonhomedetail/HomeDetailBlankFragment.kt
@@ -0,0 +1,13 @@
+package org.personal.videotogether.view.fragments.home.nestonhomedetail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.personal.videotogether.R
+
+
+class HomeDetailBlankFragment : Fragment(R.layout.fragment_home_detail_blank) {
+
+}

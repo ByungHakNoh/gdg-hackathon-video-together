16442d78a8a50265c1472111195a72568995291b
ByungHakNoh <nbh6579@naver.com>
2020-08-16 18:13:16 +0900

    [delete] MainActivity : 뒤로가기 버튼 home fragment에서 관리

--- app/src/main/java/org/personal/videotogether/MainActivity.kt
+++ app/src/main/java/org/personal/videotogether/MainActivity.kt
@@ -7,8 +7,9 @@ import androidx.activity.viewModels
 import androidx.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_main_home.view.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.personal.videotogether.view.fragments.nestonmain.HomeFragment
 import org.personal.videotogether.viewmodel.*
 
 @ExperimentalCoroutinesApi
@@ -25,27 +26,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         socketViewModel.setStateEvent(SocketStateEvent.ConnectToTCPServer)
-    }
-
-    @SuppressLint("RestrictedApi")
-    override fun onBackPressed() {
-        if (mainFragmentContainer.homeFragmentContainer != null) {
-            val homeNavController = Navigation.findNavController(mainFragmentContainer.homeFragmentContainer)
-
-            if (homeNavController.backStack.count() > 2) {
-                homeNavController.popBackStack()
-            } else {
-                killProcess()
-            }
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    private fun killProcess() {
-        moveTaskToBack(true)
-        finishAndRemoveTask()
-        // TODO : 뒤로가기 버튼으로 액티비티 스택을 지우면 SocketViewModel onCleared 에서 tcp disconnect 가 호출 되지 않음 -> 해결방안 찾기
-        socketViewModel.setStateEvent(SocketStateEvent.DisconnectFromTCPServer)
     }
 }

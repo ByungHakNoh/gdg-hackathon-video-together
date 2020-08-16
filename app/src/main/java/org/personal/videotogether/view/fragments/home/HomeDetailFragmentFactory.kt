c9f9f47092a7c7560e5bc59aef5dcdaf7e139094
ByungHakNoh <nbh6579@naver.com>
2020-08-17 00:34:39 +0900

    [add] HomeDetailFragmentFactory : 생성자가 필요한 프래그먼트 생성자 주입

--- app/src/main/java/org/personal/videotogether/view/fragments/home/HomeDetailFragmentFactory.kt
+++ app/src/main/java/org/personal/videotogether/view/fragments/home/HomeDetailFragmentFactory.kt
@@ -6,19 +6,32 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.personal.videotogether.util.view.DataStateHandler
+import org.personal.videotogether.util.view.ImageHandler
+import org.personal.videotogether.util.view.ViewHandler
+import org.personal.videotogether.view.fragments.home.nestonhomedetail.AddChatRoomFragment
+import org.personal.videotogether.view.fragments.home.nestonhomedetail.AddFriendFragment
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
 class HomeDetailFragmentFactory
 @Inject
 constructor(
-    private val dataStateHandler: DataStateHandler
+    private val dataStateHandler: DataStateHandler,
+    private val imageHandler: ImageHandler,
+    private val viewHandler: ViewHandler
 ) : FragmentFactory() {
 
     @RequiresApi(Build.VERSION_CODES.P)
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
 
         return when (className) {
+            AddFriendFragment::class.java.name -> {
+                AddFriendFragment(dataStateHandler, imageHandler, viewHandler)
+            }
+
+            AddChatRoomFragment::class.java.name -> {
+                AddChatRoomFragment(dataStateHandler)
+            }
 
             else -> super.instantiate(classLoader, className)
         }

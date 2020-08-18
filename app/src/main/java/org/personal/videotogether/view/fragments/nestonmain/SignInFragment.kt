5fe4b33275322084d705731aa792f6e2786cff01
ByungHakNoh <nbh6579@naver.com>
2020-08-18 13:07:57 +0900

    [add] SignInFragment : 로그인 시 파이어베이스 토큰 업로드

--- app/src/main/java/org/personal/videotogether/view/fragments/nestonmain/SignInFragment.kt
+++ app/src/main/java/org/personal/videotogether/view/fragments/nestonmain/SignInFragment.kt
@@ -11,47 +11,69 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_sign_in.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.withContext
 import org.personal.videotogether.R
 import org.personal.videotogether.domianmodel.UserData
 import org.personal.videotogether.util.DataState
+import org.personal.videotogether.util.SharedPreferenceHelper
 import org.personal.videotogether.util.view.DataStateHandler
 import org.personal.videotogether.util.view.ViewHandler
 import org.personal.videotogether.viewmodel.UserStateEvent
 import org.personal.videotogether.viewmodel.UserViewModel
+import java.lang.Error
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class SignInFragment
 constructor(
     private val dataStateHandler: DataStateHandler,
-    private val viewHandler: ViewHandler
+    private val viewHandler: ViewHandler,
+    private val sharedPreferenceHelper: SharedPreferenceHelper
 ) : Fragment(R.layout.fragment_sign_in), View.OnClickListener {
 
     private val TAG = javaClass.name
 
     private lateinit var mainNavController: NavController
-    private val viewModel: UserViewModel by lazy { ViewModelProvider(requireActivity())[UserViewModel::class.java] }
+    private val userViewModel: UserViewModel by lazy { ViewModelProvider(requireActivity())[UserViewModel::class.java] }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         mainNavController = Navigation.findNavController(view)
-        mainNavController.navigate(R.id.action_signInFragment_to_mainHomeFragment) // TODO : 테스트 용
+        checkAutoSignIn()
         subscribeObservers()
         setListener()
     }
 
+    // 자동 로그인 체크
+    private fun checkAutoSignIn() {
+        val isAutoSignIn = sharedPreferenceHelper.getBoolean(requireContext(), getString(R.string.auto_sign_in_key))
+
+        if (isAutoSignIn) mainNavController.navigate(R.id.action_signInFragment_to_mainHomeFragment)
+    }
+
     private fun subscribeObservers() {
         // live data : 로그인 확인
-        viewModel.signInState.observe(viewLifecycleOwner, Observer { dataState ->
+        userViewModel.signInState.observe(viewLifecycleOwner, Observer { dataState ->
             when (dataState) {
                 is DataState.Loading -> {
                     dataStateHandler.displayLoadingDialog(true, childFragmentManager)
-
                 }
+                // 다이얼로그는 파이어베이스 업로드 완료 후 제거
                 is DataState.Success<UserData?> -> {
+                    val userId = dataState.data!!.id
+                    val firebaseToken = sharedPreferenceHelper.getString(requireContext(), getString(R.string.firebase_messaging_token))
+
+                    if (autoSignInCB.isChecked) {
+                        sharedPreferenceHelper.setBoolean(requireContext(), getString(R.string.auto_sign_in_key), true)
+                    } else {
+                        sharedPreferenceHelper.setBoolean(requireContext(), getString(R.string.auto_sign_in_key), false)
+                    }
                     dataStateHandler.displayLoadingDialog(false, childFragmentManager)
+                    userViewModel.setStateEvent(UserStateEvent.UploadFirebaseToken(userId, firebaseToken!!))
                     mainNavController.navigate(R.id.action_signInFragment_to_mainHomeFragment)
                 }
                 is DataState.NoData -> {
@@ -61,6 +83,21 @@ constructor(
                 is DataState.Error -> {
                     dataStateHandler.displayLoadingDialog(false, childFragmentManager)
                     Log.i(TAG, "subscribeObservers: ${dataState.exception}")
+                }
+            }
+        })
+
+        // TODO : UserRepository Flow 에러 해결 후 사용 가능해짐
+        userViewModel.uploadFirebaseToken.observe(viewLifecycleOwner, Observer { dataState ->
+            when (dataState) {
+                is DataState.Success<Boolean?> -> {
+                    Log.i(TAG, "subscribeObservers: 파이어베이스 토큰 업로드 - 새로운 유저")
+                }
+                is DataState.DuplicatedData -> {
+                    Log.i(TAG, "subscribeObservers: 파이어베이스 토큰 업로드 - 이미 존재 Or ")
+                }
+                is DataState.Error -> {
+                    Log.i(TAG, "subscribeObservers: 파이어베이스 토큰 업로드 - 에러 발생")
                 }
             }
         })
@@ -80,7 +117,7 @@ constructor(
                 val password = passwordET.text.toString()
                 if (email.trim() != "") {
                     if (password.trim() != "") {
-                        viewModel.setStateEvent(UserStateEvent.SignIn(email, password))
+                        userViewModel.setStateEvent(UserStateEvent.SignIn(email, password))
                     } else {
                         Toast.makeText(requireContext(), "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
                     }

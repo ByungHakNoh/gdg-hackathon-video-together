67f8544fc49bfbbe7a92b7eafe62cf8a5d85f4e0
ByungHakNoh <nbh6579@naver.com>
2020-08-18 14:55:58 +0900

    [add] ViewHandler : 채팅방 이름 설정하는 메소드 추가

--- app/src/main/java/org/personal/videotogether/util/view/ViewHandler.kt
+++ app/src/main/java/org/personal/videotogether/util/view/ViewHandler.kt
@@ -5,6 +5,7 @@ import android.os.Looper
 import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
+import org.personal.videotogether.domianmodel.UserData
 
 class ViewHandler {
 
@@ -20,7 +21,28 @@ class ViewHandler {
         warningText.visibility = View.VISIBLE
 
         Handler(Looper.getMainLooper()).postDelayed({
-            warningText.visibility = View.INVISIBLE
+            warningText.visibility = View.GONE
         }, 1500)
+    }
+
+    fun formChatRoomName(participantList: List<UserData>, myUserId: Int): String {
+        val stringBuilder = StringBuilder()
+        var isFirstName = true
+
+        participantList.forEach { participant ->
+            if (myUserId != participant.id) {
+
+                if (isFirstName) {
+
+                    stringBuilder.append(participant.name)
+                    isFirstName = false
+
+                } else {
+
+                    stringBuilder.append(", ").append(participant.name)
+                }
+            }
+        }
+        return stringBuilder.toString()
     }
 }

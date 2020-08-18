20e6f0bde3e8fdcb76befea55550e2e166b87e5d
ByungHakNoh <nbh6579@naver.com>
2020-08-18 14:55:37 +0900

    [add] MyFirebaseMessagingService : 파이어베이스 토큰 받으면 쉐어드에 저장

--- app/src/main/java/org/personal/videotogether/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/org/personal/videotogether/service/MyFirebaseMessagingService.kt
@@ -3,13 +3,16 @@ package org.personal.videotogether.service
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.GsonBuilder
 import org.personal.videotogether.R
+import org.personal.videotogether.server.RequestData
+import org.personal.videotogether.server.RetrofitRequest
 import org.personal.videotogether.util.SharedPreferenceHelper
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
-class MyFirebaseMessagingService
-constructor(
-    private val SharedPreferenceHelper: SharedPreferenceHelper
-) : FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     private val TAG = javaClass.name
 
@@ -20,16 +23,20 @@ constructor(
         const val ACTION_SEND_OPEN_CHAT = "INTENT_ACTION_SEND_OPEN_CHAT"
     }
 
-    /* 새로운 토큰을 파이어베이스로부터 받으면 shared preference 에 저장 -> 서버에 userColumnID 와 함께 저장하기 위해 대기
+    /*
+    새로운 토큰을 파이어베이스로부터 받으면 shared preference 에 저장 -> 서버에 userColumnID 와 함께 저장하기 위해 대기
      */
     override fun onNewToken(token: String) {
 
         Log.i(TAG, "onNewToken : $token")
         // 새로운 토큰을 받으면 shared preference 에 저장해 놓고, 회원가입이 완료 되거나 로그인을 했을 시에 DB 에 업데이트 해준다.
-        SharedPreferenceHelper.setString(this, getText(R.string.firebase_messaging_token).toString(), token)
+        val sharedPreferenceHelper = SharedPreferenceHelper()
+
+        sharedPreferenceHelper.setString(this, getText(R.string.firebase_messaging_token).toString(), token)
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
+        Log.i(TAG, "onMessageReceived: receive")
     }
 }

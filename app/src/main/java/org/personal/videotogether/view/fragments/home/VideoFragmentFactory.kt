4c75924ef73163c9814e4e501a353c818ad83bb1
ByungHakNoh <nbh6579@naver.com>
2020-08-19 13:05:40 +0900

    [sort] 코드 정렬

--- app/src/main/java/org/personal/videotogether/view/fragments/home/VideoFragmentFactory.kt
+++ app/src/main/java/org/personal/videotogether/view/fragments/home/VideoFragmentFactory.kt
@@ -5,21 +5,17 @@ import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.personal.videotogether.util.view.DataStateHandler
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
 class VideoFragmentFactory
 @Inject
 constructor(
-    private val dataStateHandler: DataStateHandler
 ) : FragmentFactory() {
 
     @RequiresApi(Build.VERSION_CODES.P)
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
-
         return when (className) {
-
 
             else -> super.instantiate(classLoader, className)
         }

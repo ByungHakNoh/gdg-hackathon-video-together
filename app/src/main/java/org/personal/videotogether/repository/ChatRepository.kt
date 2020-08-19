94e90bd69d060b4da8f1b66327cc03449beeee70
ByungHakNoh <nbh6579@naver.com>
2020-08-19 12:57:11 +0900

    [add]ChatRepository : 채팅 서버에 업로드, 채팅 메시지 가져오기 메소드 추가

--- app/src/main/java/org/personal/videotogether/repository/ChatRepository.kt
+++ app/src/main/java/org/personal/videotogether/repository/ChatRepository.kt
@@ -3,13 +3,17 @@ package org.personal.videotogether.repository
 import android.util.Log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import org.personal.videotogether.domianmodel.ChatData
 import org.personal.videotogether.domianmodel.ChatRoomData
 import org.personal.videotogether.domianmodel.FriendData
 import org.personal.videotogether.domianmodel.UserData
+import org.personal.videotogether.room.ChatDAO
 import org.personal.videotogether.room.ChatRoomDAO
+import org.personal.videotogether.room.entity.ChatCacheMapper
 import org.personal.videotogether.room.entity.ChatRoomCacheMapper
 import org.personal.videotogether.server.RequestData
 import org.personal.videotogether.server.RetrofitRequest
+import org.personal.videotogether.server.entity.ChatMapper
 import org.personal.videotogether.server.entity.ChatRoomMapper
 import org.personal.videotogether.server.entity.FriendMapper
 import org.personal.videotogether.server.entity.UserMapper
@@ -20,8 +24,11 @@ class ChatRepository
 constructor(
     private val retrofitRequest: RetrofitRequest,
     private val chatRoomDAO: ChatRoomDAO,
+    private val chatDAO: ChatDAO,
     private val chatRoomMapper: ChatRoomMapper,
     private val chatRoomCacheMapper: ChatRoomCacheMapper,
+    private val chatMapper: ChatMapper,
+    private val chatCacheMapper: ChatCacheMapper,
     private val userMapper: UserMapper,
     private val friendMapper: FriendMapper
 ) {
@@ -94,6 +101,49 @@ constructor(
         } catch (e: Exception) {
             e.printStackTrace()
             Log.e(TAG, "postCheckEmailValid: 서버 통신 에러발생($e)")
+            emit(DataState.Error(e))
+        }
+    }
+
+    suspend fun uploadChatMessage(chatData: ChatData) {
+        try {
+            val chatEntity = chatMapper.mapToEntity(chatData)
+            val postData = HashMap<String, Any?>().apply {
+                put("chatData", chatEntity)
+            }
+            val requestData = RequestData("uploadChatMessage", postData)
+            val response = retrofitRequest.uploadChatMessage(requestData)
+
+            if (response.code() == 200) {
+                val responseChatEntity = response.body()!!
+                val responseChatData = chatMapper.mapFromEntity(responseChatEntity)
+                val chatCacheEntity = chatCacheMapper.mapToEntity(responseChatData)
+
+                chatDAO.insertChat(chatCacheEntity)
+            }
+            if (response.code() == 204) Log.i(TAG, "uploadChatMessage: 서버에 문제 발생")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.i(TAG, "uploadChatMessage: 에러 발생 - $e")
+        }
+    }
+
+    suspend fun getChatMessageList(roomId: Int): Flow<DataState<List<ChatData>>> = flow {
+        emit(DataState.Loading)
+        try {
+
+            val response = retrofitRequest.getChatMessageList("getChatMessage", roomId)
+            Log.i(TAG, "getChatMessageList: $response")
+            if (response.code() == 200) {
+                val responseChatEntityList = response.body()!!
+                val responseChatDataList = chatMapper.mapFromEntityList(responseChatEntityList)
+
+                emit(DataState.Success(responseChatDataList))
+            }
+            if (response.code() == 204) emit(DataState.NoData)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.i(TAG, "getChatMessageList: 에러 발생 - $e")
             emit(DataState.Error(e))
         }
     }

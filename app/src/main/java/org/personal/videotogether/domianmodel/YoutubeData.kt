2b1f5046dd3481165f1c680edec134819019a4a0
ByungHakNoh <nbh6579@naver.com>
2020-08-19 12:56:41 +0900

    [add] YoutubeData : Parcelize 추가

--- app/src/main/java/org/personal/videotogether/domianmodel/YoutubeData.kt
+++ app/src/main/java/org/personal/videotogether/domianmodel/YoutubeData.kt
@@ -1,8 +1,12 @@
 package org.personal.videotogether.domianmodel
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class YoutubeData (
     val channelTitle: String,
     val channelThumbnail: String,
     val title : String,
     val videoId : String
-)
+) : Parcelable

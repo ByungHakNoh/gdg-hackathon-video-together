e6f48fb14c69c19312950d920d48cbbc48859e63
ByungHakNoh <nbh6579@naver.com>
2020-08-19 13:07:59 +0900

    [sort] 코드 정렬

--- app/src/main/java/org/personal/videotogether/MyApplication.kt
+++ app/src/main/java/org/personal/videotogether/MyApplication.kt
@@ -21,7 +21,6 @@ class MyApplication : Application() {
             // 채널에 대한 기본 정보 현재는 하나의 채널만 사용하고 있다. -> 포그라운드 서비스 사용을 위해 두개로 늘림
             // 푸시 알림으로 사용자에게 알림이 온 것을 알려주기 위해 중요도를 높음으로 설정
             val importanceHigh = NotificationManager.IMPORTANCE_HIGH
-            val importanceLow = NotificationManager.IMPORTANCE_LOW
 
             // 약물 복용 시 알림
             val medicineChannel = NotificationChannel(CHAT_NOTIFICATION_CHANNEL_ID, "ChatNotification", importanceHigh)

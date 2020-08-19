3ff72c7b1633e0aefcd60413ced56af5c7c3a190
ByungHakNoh <nbh6579@naver.com>
2020-08-19 12:57:51 +0900

    [add] YoutubeRepository : 유투브 같이보기 초대 메소드 추가

--- app/src/main/java/org/personal/videotogether/repository/YoutubeRepository.kt
+++ app/src/main/java/org/personal/videotogether/repository/YoutubeRepository.kt
@@ -3,7 +3,9 @@ package org.personal.videotogether.repository
 import android.util.Log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import org.personal.videotogether.domianmodel.UserData
 import org.personal.videotogether.domianmodel.YoutubeData
+import org.personal.videotogether.server.RequestData
 import org.personal.videotogether.server.RetrofitRequest
 import org.personal.videotogether.server.entity.YoutubeMapper
 import org.personal.videotogether.util.DataState
@@ -25,7 +27,7 @@ constructor(
 
             if (response.code() == 200) {
                 val youtubeEntityList = response.body()!!
-                val youtubeList= youtubeMapper.mapFromEntityList (youtubeEntityList)
+                val youtubeList = youtubeMapper.mapFromEntityList(youtubeEntityList)
                 emit(DataState.Success(youtubeList))
             }
             if (response.code() == 204) emit(DataState.NoData)
@@ -36,6 +38,32 @@ constructor(
             // TODO: status code 안받아지는 원인 파악하기
             emit(DataState.DuplicatedData)
             emit(DataState.Error(e))
+        }
+    }
+
+    suspend fun inviteVideoTogether(inviterUserData: UserData, friendIds: List<Int>, youtubeData: YoutubeData): Flow<DataState<Boolean?>> = flow {
+        Log.i(TAG, "inviteVideoTogether: 전송?")
+        try {
+            val youtubeEntity = youtubeMapper.mapToEntity(youtubeData)
+            val postData = HashMap<String, Any?>().apply {
+                put("inviterId", inviterUserData.id)
+                put("inviterName", inviterUserData.name)
+                put("friendsIds", friendIds)
+                put("youtubeData", youtubeEntity)
+            }
+            val requestData = RequestData("inviteFriends", postData)
+            val response = retrofitRequest.inviteFriends(requestData)
+
+            if (response.code() == 200) {
+
+                emit(DataState.Success(true))
+            }
+            if (response.code() == 204) emit(DataState.NoData)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e(TAG, "postCheckEmailValid: 서버 통신 에러발생($e)")
+            // TODO: status code 안받아지는 원인 파악하기
         }
     }
 }

80c3bfc4ca5ec1b2c6fa0859f5325acca0f6bdcb
ByungHakNoh <nbh6579@naver.com>
2020-08-20 00:58:06 +0900

    [add] 유투브 같이보기 관련 데이터 추가

---
+++ app/src/main/java/org/personal/videotogether/domianmodel/PlayerStateData.kt
@@ -0,0 +1,6 @@
+package org.personal.videotogether.domianmodel
+
+data class PlayerStateData (
+    val state: String,
+    val currentSecond: Float
+)
---
+++ app/src/main/java/org/personal/videotogether/domianmodel/YoutubeJoinRoomData.kt
@@ -0,0 +1,7 @@
+package org.personal.videotogether.domianmodel
+
+data class YoutubeJoinRoomData(
+    val flag: String,
+    val userId : Int,
+    val participantCount : Int
+)
